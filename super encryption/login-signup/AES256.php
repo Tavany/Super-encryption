d8a63344d012273ba16f7bc4b5991b9af3dad290
Tavany <159811865+Tavany@users.noreply.github.com>
2024-02-13 00:29:14 +0700

    Update AES256.php

--- super encryption/login-signup/AES256.php
+++ super encryption/login-signup/AES256.php
@@ -1,6 +1,6 @@
 <?php
 
-$encryptionKey = "12345678901234567890123456789012";
+$encryptionKey = //ur key;
 $encryptionMethod = "AES-256-CBC";
 $iv = str_repeat("0", openssl_cipher_iv_length($encryptionMethod));
 
